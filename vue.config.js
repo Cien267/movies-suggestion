e7698ca7b19a84ad3724650f8f0bdfe1bc2ab67a
NGUYEN DUC KIEN <kien.nd@kiotviet.com>
2023-03-20 17:59:46 +0700

    init app

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/quiz-app-vue-v4/' // Thay tên repository của các bạn vào đây nhé
+        ? '/movies-suggestion/' // Thay tên repository của các bạn vào đây nhé
         : '/'
 }
